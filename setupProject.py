951d7d470f4288df9b999d04be7445da5dcaa5ef
Stefan Dentro <sd11@sanger.ac.uk>
2014-11-12 15:42:25 +0000

    Adding extra directories to be created for the preprocessing pipeline and removing the dirs that are no longer needed because of it

--- setupProject.py
+++ setupProject.py
@@ -5,21 +5,26 @@ from clonalityPipelineConfig import DIRICHLET_SUBDIR_NAME
 
 def setupProject(basedir):
     print("Making output directories")
+    path.joinpath(basedir,'samplesheet','input').makedirs()
+    path.joinpath(basedir,'samplesheet','output').makedirs()
     path.joinpath(basedir,'bam').makedirs()
     path.joinpath(basedir,'battenberg').makedirs()
     path.joinpath(basedir,'variants').makedirs()
-    path.joinpath(basedir,'haplotype').makedirs()
-    path.joinpath(basedir,'haplotype','mutation_loci', 'logs').makedirs()
-    path.joinpath(basedir,'haplotype','G1000', 'logs').makedirs()
+    path.joinpath(basedir,'dirichlet_preprocessing', 'input').makedirs()
+    path.joinpath(basedir,'dirichlet_preprocessing', 'output').makedirs()
     path.joinpath(basedir,'dirichlet_input','logs').makedirs()
     path.joinpath(basedir,DIRICHLET_SUBDIR_NAME,'logs').makedirs()
 
     print("")
     print("Now the following must be done:")
     print("- Create a file that lists all samplenames")
-    print("- Create a file that lists all tumournames")
     print("- Create a file that lists which samples are \"male\" or \"female\"")
+    print("- Create a file that lists where the Battenberg input can be found for these samples")
+    print("- Create a file that lists where the VCF files can be found")
+    print("- Create a file that lists tumour bam files")
+    print("- Create a file that lists normal bam files")
     print("- Put the BAM files or symlinks in the bam directory")
+    print("- Put VCF files or symlinks in the variants directory")
     print("")
 
 def main(argv):
