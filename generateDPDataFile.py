0d805d7dc7f086d1a6d7b52a74c64a7f4621ae45
Stefan Dentro <sd11@sanger.ac.uk>
2015-03-18 16:24:19 +0000

    Cleaning out old code

--- generateDPDataFile.py
+++ generateDPDataFile.py
@@ -6,12 +6,10 @@ from path import path
 from generateClonalityPipeline_util import read_sample_infile
 from clonalityPipelineConfig import RHO_AND_PSI_REGEX
 
-# def generateDPDataFile(proj_name, infile, bb_dir, dp_in_dir, run_dir):
 def generateDPDataFile(proj_name, infile, dp_in_dir, run_dir):
     ss = read_sample_infile(infile)
     
     # Collect the purity estimate for each tumour from BB output
-#     tumour2purity = getTumourPurity(ss, bb_dir, run_dir)
     tumour2purity = getTumourPurity(ss)
     
     # Create an inventory of all available dp input files
@@ -29,49 +27,6 @@ def generateDPDataFile(proj_name, infile, dp_in_dir, run_dir):
             outfile.write(sample+"\t"+tumour+"\t"+path(dp_in_file[0]).basename()+"\t"+tumour2purity[tumour]+"\n")
     outfile.close()
         
-
-# def getTumourPurity(ss, bb_dir, run_dir):
-#     ''' 
-#     Create an inventory of tumour and purity and return it as dict
-#     '''
-#     sample2purity = dict()
-#     bb_dirs = np.array(path.joinpath(bb_dir).dirs())
-#     
-#     for sample in ss.getSamplenames():
-#         
-#         normal = ss.getNormals(sample)[0]
-#         
-#         for tumour in ss.getTumours(sample): # bb is run as first normal against all tumours
-#             sample_dir = tumour
-#             
-#             # This should be fixed properly
-#             #bb_dir = ss.getBbDir(tumour)
-#         
-#             # There are two cases: 1) bb_dir contains subdirs for each sample 2) all bb output is residing in bb_dir directly
-#             if len(bb_dirs) > 0 and len(bb_dirs[np.array([sample_dir in item for item in bb_dirs])]) == 1:
-#                 # Case 1
-#                 indir = path.joinpath(bb_dir, tumour)
-#             elif len(bb_dirs) > 0 and len(bb_dirs[np.array([sample_dir in item for item in bb_dirs])]) > 1:
-#                 print("Found more than one bb output dir for sample "+tumour)
-#                 sys.exit(1)
-#             else:
-#                 # Case 2
-#                 indir = bb_dir
-#             
-#         listing = np.array(path(indir).listdir(RHO_AND_PSI_REGEX))
-#         if len(listing) == 1:
-#             rho_and_psi = listing[np.array([tumour in filename for filename in listing])] # Should yield only a single file
-#         else:
-#             print("Found different than expected number of matches for "+tumour)
-#             continue
-#         if not len(rho_and_psi) == 1:
-#             print("Found different than expected number of matches for "+tumour)
-#             continue
-#             
-#         sample2purity[tumour] = getPurityPerSample(rho_and_psi[0])
-#         
-#     return sample2purity
-
 def getTumourPurity(ss):
     '''
     Fetches the rho_and_psi file for each tumour_id, for each sample
@@ -116,7 +71,6 @@ def main(argv):
     parser = argparse.ArgumentParser(prog='GenerateDPDataFile',
                              formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", required=True, type=str, help="Full path to a sample sheet")
-    #parser.add_argument("-b", required=True, type=str, help="Full path to where Battenberg output is stored")
     parser.add_argument("-d", required=True, type=str, help="Full path to where Dirichlet input files are stored")
     parser.add_argument("-r", required=True, type=str, help="Full path to directory where the pipeline is going to be run.")
     parser.add_argument("-p", required=True, type=str, help="Name of the project.")
@@ -124,7 +78,6 @@ def main(argv):
 #     parser.add_argument("--no_iters_burn_in", type=int, help="Number of iterations used for burn in.")
     
     args = parser.parse_args()
-     #generateDPDataFile(args.p, args.i, args.b, args.d, args.r)
     generateDPDataFile(args.p, args.i, args.d, args.r)
     
 if __name__ == '__main__':
